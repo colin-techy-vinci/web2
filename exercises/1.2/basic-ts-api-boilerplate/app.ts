f02bf91e10c4d1182a3c17695ad2673428832885
colin-techy-vinci <you@example.com>
2025-09-15 13:30:28 +0200

    new: ex1.2µ

---
+++ exercises/1.2/basic-ts-api-boilerplate/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+import filmRouter from "./routes/films";
+
+const app = express();
+let i = 0;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/films", filmRouter);
+
+app.use((_req, _res, next) =>{
+    i++;
+    console.log("GET counter: " + i);
+    next();
+})
+
+export default app;
