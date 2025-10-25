32f2ba167c9ae12222462c994aec9df4ffd9b55d
colin-techy-vinci <ctechy@DESKTOP-2O4C5NO.localdomain>
2025-10-25 18:47:34 +0200

    new:ex2.4

--- exercises/2.1-2-3-4/src/components/App/index.tsx
+++ exercises/2.1-2-3-4/src/components/App/index.tsx
@@ -1,5 +1,7 @@
 import Cinema from "../Cinema";
 import PageTitle from "../PageTitle";
+import Footer from "../Footer";
+import Header from "../Header";
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
 
@@ -46,11 +48,17 @@ const App = () => {
   ];
   return (
     <div>
+      <Header url="https://media.istockphoto.com/id/1202770152/fr/photo/bobine-de-film-disolement-sur-le-fond-jaune-lumineux-dans-les-couleurs-pastel.jpg?s=1024x1024&w=is&k=20&c=2yKBrC8oyimPdW-5IxFWN_zxFPVK3KWYL9OE2gVmVX4=">
+        <h1>hello test</h1>
+      </Header>
       <PageTitle title={pageTitle} />
 
       <Cinema name={cinema1Name} movies={moviesCinema1} />
 
       <Cinema name={cinema2Name} movies={moviesCinema2} />
+      <Footer url="https://media.istockphoto.com/id/1202770152/fr/photo/bobine-de-film-disolement-sur-le-fond-jaune-lumineux-dans-les-couleurs-pastel.jpg?s=1024x1024&w=is&k=20&c=2yKBrC8oyimPdW-5IxFWN_zxFPVK3KWYL9OE2gVmVX4=">
+        <p>Hello film</p>
+      </Footer>
     </div>
   );
 };
