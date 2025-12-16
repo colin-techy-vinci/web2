f4b37390716b09737ae050514e2a6c622f26ad58
colin-techy-vinci <ctechy@DESKTOP-2O4C5NO.localdomain>
2025-12-16 16:12:07 +0100

    new:ex2.14b-refactor

---
+++ exercises/2.14b/src/RandomDog.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+interface Dog {
+    message: string,
+    status: string
+}
+const RandomDog = () => {
+    const [dogImage, setDogImage] = useState<Dog | undefined>()
+    useEffect(() => {
+        fetchDogImage();
+    }, [])
+    const fetchDogImage = async () => {
+        try {
+            const response = await fetch("https://dog.ceo/api/breeds/image/random");
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            const data = await response.json();
+            setDogImage(data);
+        } catch (error) {
+            console.error("There was a problem with the fetch operation:", error);
+        }
+    }
+    console.log(dogImage);
+    return (
+        <div>
+            <h1>Random Dog Image</h1>
+            {dogImage && <img src={dogImage.message} alt="A Random Dog" />}
+        </div>
+    )
+}
+export default RandomDog
